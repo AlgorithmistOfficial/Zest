b239e1ea1003cedf933a86ed62c1c6aff32da97e
Shreyansh Srivastava <shreyansh.official.6726@gmail.com>
2026-06-28 13:55:51 +0530

    work

--- frontend/admin-portal/src/api.js
+++ frontend/admin-portal/src/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const baseURL = process.env.REACT_APP_BACKEND_URL;
+const baseURL = `${process.env.REACT_APP_BACKEND_URL || 'http://localhost:7860'}/api`;
 
 const api = axios.create({
     baseURL
